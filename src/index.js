b336ad05a2dfa819ecc93ed365ebaabad83d7523
RyotaIwahashi <r-iwahashi@groovenauts.jp>
2023-02-27 20:36:07 +0900

    @reduxjs/toolkitを使用する

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
+// const counterReducer = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//     case 'ZERO':
+//       return 0
+//     default:
+//       return state
+//   }
+// }
+
+// createSliceを使って上記を書き換える
+// createSlice は下記を返す関数
+// {
+//    name : string,
+//    reducer : ReducerFunction,
+//    actions : Object<string, ActionCreator>,
+// }
+const counterSlice = createSlice({
+  name: 'counter', // reducers名
+  initialState: 0, // initialState
+  reducers: { // reducers
+    increment: state => state + 1,
+    decrement: state => state - 1,
+    zero: () => 0
   }
-}
+})
 
-const store = createStore(counterReducer)
+// createStore は、 @reduxjs/toolkit パッケージの configureStore メソッドを代わりに使用するのが良い。
+// Redux Toolkit は、ストアのセットアップ、リデューサー、データ フェッチなどを含む、Reduxロジックを記述するための推奨されるアプローチ
+// const store = createStore(counterReducer)
+const store = configureStore({
+  reducer: counterSlice.reducer
+})
+// configureStore を使用、middleware や devTools の設定済み
 
 const App = () => {
   return (
@@ -25,17 +49,17 @@ const App = () => {
         {store.getState()}
       </div>
       <button 
-        onClick={e => store.dispatch({ type: 'INCREMENT' })}
+        onClick={e => store.dispatch(counterSlice.actions.increment())}
       >
         plus
       </button>
       <button
-        onClick={e => store.dispatch({ type: 'DECREMENT' })}
+        onClick={e => store.dispatch(counterSlice.actions.decrement())}
       >
         minus
       </button>
       <button 
-        onClick={e => store.dispatch({ type: 'ZERO' })}
+        onClick={e => store.dispatch(counterSlice.actions.zero())}
       >
         zero
       </button>
